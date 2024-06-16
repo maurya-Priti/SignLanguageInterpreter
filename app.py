ea51b8743faa419e88c9add3fb4618f878a7b836
Priti Maurya <maurya.priti032001@gmail.com>
2024-06-16 10:34:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -98,65 +98,6 @@ def process_frame():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-# def gen_frames():
-
-#   cap = cv2.VideoCapture(0)
-
-#   hands = mp_hands.Hands(
-#       min_detection_confidence=0.7, min_tracking_confidence=0.5)
-
-#   while cap.isOpened():
-#     success, image = cap.read()
-#     if not success:
-#       print("Ignoring empty camera frame.")
-#       # If loading a video, use 'break' instead of 'continue'.
-#       continue
-
-#     # Flip the image horizontally for a later selfie-view display, and convert
-#     # the BGR image to RGB.
-#     image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-#     # To improve performance, optionally mark the image as not writeable to
-#     # pass by reference.
-#     image.flags.writeable = False
-#     results = hands.process(image)
-    
-#     # Draw the hand annotations on the image.
-#     image.flags.writeable = True
-#     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    
-#     if results.multi_hand_landmarks:
-#       points=[]
-#       for hand_landmarks in results.multi_hand_landmarks:
-#         for i in range(21):
-#             x=hand_landmarks.landmark[i].x
-#             y=hand_landmarks.landmark[i].y
-#             points.append(x)
-#             points.append(y)  
-#       # print(points)
-#       if len(points)==42:
-#               predict=forrest_model.predict([points])
-#       #print(predict)    
-              
-#       for hand_landmarks in results.multi_hand_landmarks:
-#         mp_drawing.draw_landmarks(
-#             image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
-#     if results.multi_hand_landmarks:
-          
-#         cv2.putText(image, '%s' % (predict), (100,400), cv2.FONT_HERSHEY_SIMPLEX, 4, (255,255,255), 4)
-      
-#     # cv2.imshow('MediaPipe Hands', image)
-#     # if cv2.waitKey(5) & 0xFF == 27:
-#     #   break
-
-#     ret, buffer = cv2.imencode('.jpg', image)
-#     image = buffer.tobytes()
-#     yield (b'--frame\r\n'
-#                    b'Content-Type: image/jpeg\r\n\r\n' + image + b'\r\n')
-
-
-# @app.route('/video_feed')
-# def video_feed():
-#     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 
@@ -165,4 +106,4 @@ def process_frame():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
